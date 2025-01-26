34d64c7dc26060c29115680507e64bde755c3085
SabinZibileanu <s_zibileanu@yahoo.com>
2025-01-26 19:16:24 +0200

    problem solutions

---
+++ problems-solutions/Happy Number.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def power_of_2_sum(self, n: int) -> int:
+        sum_p2 = 0
+
+        while n != 0:
+            sum_p2 += (n % 10) ** 2
+            n //= 10
+        
+        return sum_p2
+
+    def isHappy(self, n: int) -> bool:
+        ok = True
+        duplicates_check = []
+
+        while ok:
+            print(n)
+            p2_sum = self.power_of_2_sum(n)
+            if p2_sum not in duplicates_check:
+                if p2_sum == 1:
+                    break
+                
+                else:
+                    duplicates_check.append(p2_sum)
+                    n = p2_sum
+
+            else:
+                ok = False
+
+        return ok
+
---
+++ problems-solutions/Word Pattern.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> None:
+        matching_pattern = {}
+        split_words = s.split()
+        unique_letters = set([letter for letter in pattern])
+        unique_words = set(split_words)
+
+        if len(unique_letters) != len(unique_words):
+            return False
+
+        for letter, word in zip(pattern, split_words):
+            if letter not in matching_pattern:
+                matching_pattern[letter] = word
+            
+            elif matching_pattern[letter] != word:
+                return False
+        return True
+
+                    
+        
+
+s = Solution()
+print(s.wordPattern('aba','dog cat cat'))
